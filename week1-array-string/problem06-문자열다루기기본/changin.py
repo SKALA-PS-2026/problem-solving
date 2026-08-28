6080e5dd5d57ae85e84db577071299898958cbe7
kochangin-ai <kci1761@naver.com>
2026-08-28 09:36:16 +0900

    feat: week1 문제 9,6번 코드 수정 안했던 것 다시 넣었습니다.

--- week1-array-string/problem06-문자열다루기기본/changin.py
+++ week1-array-string/problem06-문자열다루기기본/changin.py
@@ -4,17 +4,14 @@
 풀이 방법: 문자열 s의 길이가 4 또는 6인지 먼저 검사한 뒤, for문으로 각 문자의 ASCII 코드 값(ord)을 확인해 모두 숫자 범위('0'~'9', 48~57)에 해당하는지 검사
 '''
 
-def solution(arr):
-    answer = []
-    prev = -1
-    for c in arr:
-        if prev == -1 :
-            prev = c 
-            answer.append(c)
-        elif prev == c :
-            continue
-        else:
-            prev = c
-            answer.append(c)
-    return answer
-
+def solution(s):
+    if len(s) == 6 or len(s)== 4:
+        for c in s:
+            print(c)
+            if 48 <=ord(c) <= 57:
+                continue
+            else:
+                return False
+        return True
+    else:
+        return False
--- week1-array-string/problem09-시저암호/changin.py
+++ week1-array-string/problem09-시저암호/changin.py
@@ -4,15 +4,20 @@
 풀이 방법: for문으로 문자열 s의 각 문자를 순회하며 공백 여부 및 대/소문자 ASCII 코드 범위(ord)를 체크하여 n만큼 밀고 26을 빼주는 방식으로 처리
 '''
 
-def solution(s):
+def solution(s, n):
     answer = ''
-    for word in s.split(' '):
-        for i in range(len(word)):
-            if i % 2 == 0:
-                answer = answer + word[i].upper()
-            else:
-                print(word[i].lower())
-                answer = answer + word[i].lower()
-        answer = answer + ' '
-    return answer[:-1]
+    for c in s:
+        if c == ' ':
+            answer = answer + c
+        elif ord(c)<=90:
+            a = ord(c) + n
+            if a > 90:
+                a -= 26
+            answer = answer + chr(a)
+        elif ord(c)>=97:
+            a = ord(c) + n
+            if a > 122:
+                a -= 26
+            answer = answer + chr(a)
+    return answer
 
